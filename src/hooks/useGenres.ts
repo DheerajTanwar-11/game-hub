4742659e57e348a88b32304386cebe1dab10b971
Ayush @ Classiebit <ayush.classiebit@gmail.com>
2023-04-25 15:11:20 +0530

    chore: use common api-client

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 
+const apiClient = new APIClient<Genre>("/genres");
 export interface Genre {
   id: number;
   name: string;
@@ -12,8 +13,7 @@ export interface Genre {
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 1000 * 60 * 60 * 24, // 24 hours
   });
 
