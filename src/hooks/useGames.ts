1c5296e25280b9352674dd431c93b4084eb86628
Ayush @ Classiebit <ayush.classiebit@gmail.com>
2023-04-25 13:49:09 +0530

    chore: use react-query to fetch data

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,57 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 
 export interface Platform {
-	id: number;
-	slug: string;
-	name: string;
+  id: number;
+  slug: string;
+  name: string;
 }
 
 export interface Game {
-	id: number;
-	name: string;
-	slug: string;
-	released: string;
-	tba: boolean;
-	background_image: string;
-	rating: number;
-	rating_top: number;
-	ratings: [];
-	ratings_count: number;
-	reviews_text_count: number;
-	added: number;
-	added_by_status: {};
-	metacritic: number;
-	playtime: number;
-	suggestions_count: number;
-	updated: string;
-	user_game: null;
-	reviews_count: number;
-	saturated_color: string;
-	dominant_color: string;
-	platforms: [];
-	parent_platforms: { platform: Platform }[];
-	genres: [];
-	stores: [];
-	clip: null;
-	tags: [];
-	esrb_rating: {};
-	short_screenshots: [];
+  id: number;
+  name: string;
+  slug: string;
+  released: string;
+  tba: boolean;
+  background_image: string;
+  rating: number;
+  rating_top: number;
+  ratings: [];
+  ratings_count: number;
+  reviews_text_count: number;
+  added: number;
+  added_by_status: {};
+  metacritic: number;
+  playtime: number;
+  suggestions_count: number;
+  updated: string;
+  user_game: null;
+  reviews_count: number;
+  saturated_color: string;
+  dominant_color: string;
+  platforms: [];
+  parent_platforms: { platform: Platform }[];
+  genres: [];
+  stores: [];
+  clip: null;
+  tags: [];
+  esrb_rating: {};
+  short_screenshots: [];
 }
 
 const useGames = (gameQuery: GameQuery) =>
-	useData<Game>(
-		"/games",
-		{
-			params: {
-				genres: gameQuery.genre?.id,
-				platforms: gameQuery.platform?.id,
-				ordering: gameQuery.sortOrder,
-				search: gameQuery.search,
-			},
-		},
-		[gameQuery]
-	);
+  useQuery({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.search,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
