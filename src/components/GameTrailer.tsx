591ef31bd4fe1661045dfb415a87d4c242153b75
ayusshrathore <ayusshrathore@gmail.com>
2023-04-26 19:45:51 +0530

    chore: fix `data.results` being undefined

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -2,23 +2,25 @@ import { Spinner } from "@chakra-ui/react";
 import useTrailers from "../hooks/useTrailers";
 
 interface Props {
-  id: number;
+	id: number;
 }
 
 const GameTrailer = ({ id }: Props) => {
-  const { data, isLoading, error } = useTrailers(id);
+	const { data, isLoading, error } = useTrailers(id);
 
-  if (isLoading) return <Spinner />;
+	if (isLoading) return <Spinner />;
 
-  if (error || !data) throw error;
+	if (error || !data) throw error;
 
-  return (
-    <video
-      controls
-      src={data.results[0].data.max}
-      poster={data.results[0].preview}
-    />
-  );
+	const first = data.results[0];
+
+	return first ? (
+		<video
+			controls
+			src={data.results[0].data.max}
+			poster={data.results[0].preview}
+		/>
+	) : null;
 };
 
 export default GameTrailer;
